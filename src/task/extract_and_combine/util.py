26dbdcfa86b4ed10b8a70f630e2b7cdd6ef1d12b
fahmi-aa <88307266+fahmi-aa@users.noreply.github.com>
2022-02-13 13:28:03 +0700

    feat: util file

---
+++ src/task/extract_and_combine/util.py
@@ -0,0 +1,16 @@
+from os import listdir
+from os.path import isfile, join
+
+def get_files_in_dir(path: str):
+    return [f for f in listdir(path) if isfile(join(path, f))]
+
+def combine_all_file(dir: str, out: str):
+    joined_file = open(out, "a+")
+    file_paths = get_files_in_dir(dir)
+
+    for file_path in file_paths:
+        with open(join(dir, file_path)) as f:
+            payload = f.read()
+            joined_file.write(payload+'\n')
+
+    joined_file.close()
