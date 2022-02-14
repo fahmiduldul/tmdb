fa57a700e1d5fd77884fd870047f646fd829f28a
fahmi-aa <88307266+fahmi-aa@users.noreply.github.com>
2022-02-14 22:05:48 +0700

    ref: remove repetitive code

--- src/task/transform/dimension_tables.py
+++ src/task/transform/dimension_tables.py
@@ -9,15 +9,30 @@ spark = SparkSession.builder.appName("tmdb") \
 df_movies = spark.read.json("gs://de-porto/qoala/raw_data/movies_joined.json")
 df_series = spark.read.json("gs://de-porto/qoala/raw_data/series_joined.json")
 
-# Transform genres and place in GCS
-df_series_gen = df_series.select(F.explode(F.col("genres"))).select(F.col("col.*")) \
-    .withColumnRenamed("id", "series_id") \
-    .withColumnRenamed("name", "series_genre") \
-    .distinct()
-df_movies_gen = df_movies.select(F.explode(F.col("genres"))).select(F.col("col.*")) \
-    .withColumnRenamed("id", "movies_id") \
-    .withColumnRenamed("name", "movies_genre") \
-    .distinct()
+
+def explode_and_spread_column(df, column: str):
+    return df.select(F.explode(F.col(column))).select(F.col("col.*"))
+
+
+def rename_columns(df, current_cols, new_cols):
+    if len(current_cols) != len(new_cols):
+        raise Exception()
+
+    for i in range(len(current_cols)):
+        df = df.withColumnRenamed(current_cols[i], new_cols[i])
+
+    return df
+
+## Transform and load genres dimenstion table
+df_series_gen = rename_columns(
+        explode_and_spread_column(df_series, "genres"),
+        ["id", "name"], ["series_id", "movies_name"]
+    ).distinct()
+
+df_movies_gen = rename_columns(
+        explode_and_spread_column(df_movies, "genres"),
+        ["id", "name"], ["movies_id", "movies_name"]
+    ).distinct()
 
 genre_comparison = df_series_gen \
     .join(df_movies_gen, df_series_gen.series_id == df_movies_gen.movies_id, "full")
@@ -30,19 +45,16 @@ dwh_genres = genre_comparison \
 dwh_genres.write.parquet("gs://de-porto/qoala/genres.parquet")
 
 
-df_series_com = df_series.select(F.explode(F.col("production_companies"))).select(F.col("col.*")) \
-    .select(["id", "name", "origin_country"]) \
-    .withColumnRenamed("id", "series_id") \
-    .withColumnRenamed("name", "series_company") \
-    .withColumnRenamed("origin_country", "series_origin_country") \
-    .distinct()
+## Transform and load production_companies dimenstion table
+df_series_com = rename_columns(
+        explode_and_spread_column(df_series, "production_companies"),
+        ["id", "name", "origin_country"], ["series_id", "series_name", "series_origin_country"]
+    ).distinct()
 
-df_movies_com = df_movies.select(F.explode(F.col("production_companies"))).select(F.col("col.*")) \
-    .select(["id", "name", "origin_country"]) \
-    .withColumnRenamed("id", "movies_id") \
-    .withColumnRenamed("name", "movies_company") \
-    .withColumnRenamed("origin_country", "movies_origin_country") \
-    .distinct()
+df_movies_com = rename_columns(
+        explode_and_spread_column(df_movies, "production_companies"),
+        ["id", "name", "origin_country"], ["movies_id", "movies_name", "movies_origin_country"]
+    ).distinct()
 
 companies_comparison = df_series_com \
     .join(df_movies_com, df_series_com.series_id == df_movies_com.movies_id, "full")
@@ -55,4 +67,3 @@ dwh_companies = companies_comparison \
     .where(F.col("origin_country").isNotNull())
 
 dwh_companies.write.parquet("gs://de-porto/qoala/companies.parquet")
-
