8b385d8e2303b40b1628e724b7f64b7116a71190
fahmi-aa <88307266+fahmi-aa@users.noreply.github.com>
2022-02-16 23:29:40 +0700

    fix: poke and timeout

--- src/airflow_dag/dag.py
+++ src/airflow_dag/dag.py
@@ -58,8 +58,9 @@ with DAG("tmdb", schedule_interval="@weekly", start_date=dt.datetime(2022, 1, 1)
                 task_id=f"wait_{file_.split('.')[0]}",
                 bucket=PROJECT_ID,
                 object=f"qoala/raw_data/{file_}",
-                poke_interval=90,
-                timeout=20 * 60
+                poke_interval=30,
+                timeout=20 * 60,
+                retries=50
             )
 
 
